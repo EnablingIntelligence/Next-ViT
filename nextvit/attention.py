09bee54e81e21a28cecbba0df6cb4f1c5a49a6e0
Christopher Schicho <57227765+c-schicho@users.noreply.github.com>
2024-02-22 17:08:23 +0100

    #2-implement ntb module (#8)

--- nextvit/attention.py
+++ nextvit/attention.py
@@ -1,4 +1,5 @@
 import torch
+from einops import rearrange
 from torch import nn
 
 
@@ -20,3 +21,58 @@ class MHCA(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.mhca(x)
+
+
+class EMHSA(nn.Module):
+    """
+    Efficient Multi-Head Self-Attention
+    https://arxiv.org/pdf/2207.05501.pdf
+    """
+
+    def __init__(self, in_features: int, spatial_reduction_ratio: int, n_heads: int = 8, hidden_dim: int = 64):
+        super().__init__()
+        self.in_dim = in_features
+        self.n_heads = n_heads
+        self.hidden_dim = hidden_dim
+        self.scaling_factor = hidden_dim ** (-0.5)
+
+        self.norm = nn.BatchNorm2d(num_features=in_features)
+        self.avg_pool = nn.AvgPool2d(kernel_size=spatial_reduction_ratio, stride=spatial_reduction_ratio)
+        self.q_proj = nn.Linear(in_features=in_features, out_features=hidden_dim)
+        self.k_proj = nn.Linear(in_features=in_features, out_features=hidden_dim)
+        self.v_proj = nn.Linear(in_features=in_features, out_features=hidden_dim)
+        self.out_proj = nn.Linear(in_features=hidden_dim * n_heads, out_features=in_features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, c, h, w = x.shape
+        x = self.norm(x)
+        x_reshaped = x.view(b, c, h * w).permute(0, 2, 1)
+
+        # query
+        q = (self.q_proj(x_reshaped)
+             .view(b, h * w, self.n_heads, self.hidden_dim)
+             .permute(0, 2, 1, 3).contiguous())
+
+        # key
+        k = (self.k_proj(x_reshaped)
+             .view(b, self.n_heads * self.hidden_dim, h, w))
+        k = self.avg_pool(k)
+        k = rearrange(k, "b (head n) h w -> b head (h w) n", head=self.n_heads)
+
+        # value
+        v = (self.v_proj(x_reshaped)
+             .view(b, self.n_heads * self.hidden_dim, h, w))
+        v = self.avg_pool(v)
+        v = rearrange(v, "b (head n) h w -> b head (h w) n", head=self.n_heads)
+
+        # attention
+        scores = torch.einsum('... i d, ... j d -> ... i j', q, k) * self.scaling_factor
+        weights = torch.softmax(scores, dim=-1)
+
+        attention = (torch.matmul(weights, v)
+                     .permute(0, 2, 1, 3)
+                     .contiguous()
+                     .view(b, h * w, self.n_heads * self.hidden_dim))
+
+        out = self.out_proj(attention).view(b, self.in_dim, h, w)
+        return out + x
---
+++ nextvit/ntb.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+from nextvit.attention import MHCA, EMHSA
+from nextvit.mlp import MLP
+
+
+class NTB(nn.Module):
+    """
+    Next Transformer Block
+    https://arxiv.org/pdf/2207.05501.pdf
+    """
+
+    def __init__(self, in_features: int, out_features: int, shrink_ratio: float = 0.75,
+                 spatial_reduction_ratio: int = 1, mlp_expansion_ratio: int = 1):
+        super().__init__()
+        emhsa_features = int(in_features * shrink_ratio)
+        mhca_features = out_features - emhsa_features
+
+        self.conv1 = nn.Conv2d(in_channels=in_features, out_channels=emhsa_features, kernel_size=1)
+        self.conv2 = nn.Conv2d(in_channels=emhsa_features, out_channels=mhca_features, kernel_size=1)
+        self.emhsa = EMHSA(emhsa_features, spatial_reduction_ratio)
+        self.mhca = MHCA(mhca_features)
+        self.mlp = MLP(out_features, out_features, mlp_expansion_ratio)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        emhsa_in = self.conv1(x)
+        emhsa_out = self.emhsa(emhsa_in) + emhsa_in
+        mhca_in = self.conv2(emhsa_out)
+        mhca_out = self.mhca(mhca_in) + mhca_in
+        mlp_in = torch.cat([emhsa_out, mhca_out], dim=1)
+        return self.mlp(mlp_in) + mlp_in
