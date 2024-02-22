f7bb2f4bf618bbdbdec72c50243907f075e92b34
christopher schicho <christopher.schicho@gmail.com>
2024-02-22 18:49:07 +0100

    #3-implement next-vit

---
+++ nextvit/block.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+
+from nextvit.ncb import NCB
+from nextvit.ntb import NTB
+
+
+class NextViTBlock(nn.Module):
+    """
+    Next Vision Transformer Block
+    https://arxiv.org/pdf/2207.05501.pdf
+    """
+
+    def __init__(self, in_features: int, out_features: int, num_ncb_layers: int, num_ntb_layers: int, depth: int = 1,
+                 spatial_reduction_ratio: int = 1):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.num_ncb_layers = num_ncb_layers
+        self.num_ntb_layers = num_ntb_layers
+        self.depth = depth
+        self.spacial_reduction_ratio = spatial_reduction_ratio
+
+        self.block = self.__make_block_layers()
+
+    def __make_block_layers(self) -> nn.Sequential:
+        block = []
+
+        for depth_idx in range(self.depth):
+            is_last_layer = depth_idx == self.depth - 1
+            out_features = self.out_features if is_last_layer else self.in_features
+            block.append(self.__make_layer(out_features))
+
+        return nn.Sequential(*block)
+
+    def __make_layer(self, out_features: int) -> nn.Sequential:
+        return nn.Sequential(
+            *[
+                *[NCB(self.in_features, self.in_features) for _ in range(self.num_ncb_layers)],
+                *[NTB(self.in_features, out_features, self.spacial_reduction_ratio) for _ in range(self.num_ntb_layers)]
+            ]
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.block(x)
---
+++ nextvit/nextvit.py
@@ -0,0 +1,50 @@
+from enum import Enum
+
+import torch
+from torch import nn
+
+from nextvit.block import NextViTBlock
+from nextvit.embedding import PatchEmbedding
+from nextvit.stem import Stem
+
+
+class NextViTType(Enum):
+    SMALL = 2
+    BASE = 4
+    LARGE = 6
+
+
+class NextViT(nn.Module):
+    """
+    Next Vision Transformer
+    https://arxiv.org/pdf/2207.05501.pdf
+    """
+
+    def __init__(self, in_features: int, out_features: int, num_classes: int, vit_type: NextViTType = NextViTType.BASE):
+        super().__init__()
+        self.stem = Stem(in_features)
+
+        self.blocks = nn.Sequential(
+            # stage 1
+            PatchEmbedding(in_features=64, out_features=96, pooling=False),
+            NextViTBlock(in_features=96, out_features=96, num_ncb_layers=1, num_ntb_layers=0),
+            # stage 2
+            PatchEmbedding(in_features=96, out_features=192),
+            NextViTBlock(in_features=192, out_features=256, num_ncb_layers=3, num_ntb_layers=1),
+            # stage 3
+            PatchEmbedding(in_features=256, out_features=384),
+            NextViTBlock(in_features=384, out_features=512, num_ncb_layers=4, num_ntb_layers=1, depth=vit_type.value),
+            # stage 4
+            PatchEmbedding(in_features=512, out_features=768),
+            NextViTBlock(in_features=768, out_features=1024, num_ncb_layers=2, num_ntb_layers=1),
+        )
+
+        self.batch_norm = nn.BatchNorm2d(num_features=out_features)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.out_proj = nn.Linear(1, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        stem_out = self.stem(x)
+        blocks_out = self.blocks(stem_out)
+        pool_out = self.avg_pool(blocks_out)
+        return self.out_proj(torch.flatten(pool_out, 1))
